5ad40c0a93e71622803bd27ab70681af3e09bab9
AlexandreMFilho <alexmaimartfilho@gmail.com>
2019-12-17 23:57:57 -0300

    projeto final 1 esboço

---
+++ Projeto Final/jcript.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //Inicialize aquivar
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
